e3d49c825308167c26deb917de240f8abf2bdc77
Katya Miranda <katyamiranda03@hotmail.com>
2023-05-31 09:31:25 -0600

    agregue icono de carrito de compras y agreue estilo

---
+++ src/components/CarWidget/CarWidget.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+
+const CarWidget = () => {
+    return (
+        <div className="car">
+            <ShoppingCartIcon />
+            <p> 3</p>
+        </div>
+    );
+};
+
+export default CarWidget
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,21 +1,27 @@
-import React from 'react'
+import React from 'react';
+import CarWidget from '../CarWidget/CarWidget'
 
 const NavBar = () => {
 
     return (
-        <nav className="navbar sticky-top justify-content-center"> 
-    <ul className="nav">
-        <li className="nav-item">
-            <a className="nav-link" href="#"> Inicio </a>
-        </li>
-        <li className="nav-item">
-            <a className="nav-link" href="#"> Vinilos </a>
-        </li>
-        <li className="nav-item">
-            <a className="nav-link" href="#"> Sobre nosotros </a>
-        </li>
-    </ul>
-        </nav>
+<nav className="navbar sticky-top"> 
+    <div className="container justify-content-center">
+        <ul className="nav">
+            <li className="nav-item">
+                <a className="nav-link" href="#"> Inicio </a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link" href="#"> Vinilos </a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link" href="#"> Sobre nosotros </a>
+            </li>
+        </ul>
+            <div>
+        <CarWidget />
+            </div>
+    </div>
+</nav>
     )
 }
 
