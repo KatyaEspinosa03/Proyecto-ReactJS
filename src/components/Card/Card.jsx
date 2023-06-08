a9fccb2cae370ed7f3ba41b4cdd92f5f9a7911a8
Katya Miranda <katyamiranda03@hotmail.com>
2023-06-08 15:25:18 -0600

    corregí el archivo de card para agregar un card-footer y agregue botones al footer

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -4,15 +4,33 @@ import React from 'react'
 export const Card = ({card}) => {
   return (
 
-            <div className="card" style={{width: "18rem"}}>
-                <img className="card-img-top" src={card.image} alt="Card image cap" />
-                <div className="card-body">
-                    <h5 className="card-title">{card.albumName}</h5>
-                    <p className="card-text">{card.artistName} </p>
-                    <p className="card-text"> {card.price}</p>
-                    <a href="#" className="btn">Go somewhere</a>
-                </div>
-            </div>
+
+<div className='card-group'>
+    <div className='card'>
+      <img className="card-img-top" src={card.image} alt="Card image cap" />
+      <div className='card-body'>
+        <h5 className='card-title'>{card.albumName}</h5>
+        <p className='card-text'> {card.artistName} </p>
+        <p className='card-text'> {card.price} </p>
+      </div>
+    <div className='card-footer'>
+      <div className='row button'>
+        <div className='col justify-content-center'>
+        <button type="button" className="btn">+</button>
+        </div>
+        <div className='col justify-content-center'>
+        <h5 className="card-title"> 1 </h5>
+        </div>
+        <div className='col justify-content-center'>
+        <button type="button" className="btn">-</button>
+        </div>
+
+      </div>
+
+    </div>
+  </div>
+
+</div>
 
 
   )
