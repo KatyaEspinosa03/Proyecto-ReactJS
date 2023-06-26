0a215a7b6d289100dc89bde535a47b13b6de2265
Katya Miranda <katyamiranda03@hotmail.com>
2023-06-25 18:04:13 -0600

    cree la funcioalidad de mi componente de busqueda para que me muestre en consola los objetos que coinciden con el nombre ingresado

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import {useState} from 'react';
 import CarWidget from '../CarWidget/CarWidget'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
@@ -10,6 +10,38 @@ import Button from 'react-bootstrap/Button';
 
 const NavBar = () => {
 
+    const [value,setValue] = useState("");
+    const [userData, setUserData] = useState({});
+
+    console.log(userData)
+
+    const handleOnChange = (e) => {
+        setValue(e.target.value);
+    }
+const handleOnSubmit = (e) => {
+    e.preventDefault()
+
+    // Hago que cada palabra que el usuario ingrese tenga la primera letra en mayúscula para que coincida con el json
+    const userInput = value.toLowerCase().split(' ').map(word => {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    })
+    .join(' ');
+
+   console.log(userInput)
+
+   // hago la llamada al json y pido que lo encuentre de acuerdo a la propiedad de artistName
+   fetch("./data.json")
+   .then((response) => response.json())
+   .then ((data) => {
+    const foundItems = data.filter((item) => item.artistName === userInput);
+    if (foundItems.length > 0){
+        setUserData(foundItems);
+    }else{
+        console.log("Item not found");
+    }
+   })
+   .catch((error) => console.error(error));
+}
     return (
 <Navbar className="navbar sticky-top"> 
     <Container className="justify-content-center nav">
@@ -21,14 +53,20 @@ const NavBar = () => {
               </div>
             </NavDropdown>
             <Nav.Link as={NavLink} to="/about" className='nav nav-link'>Sobre nosotros</Nav.Link>
-            <Form className="d-flex form">
+            <Form className="d-flex form" onSubmit={handleOnSubmit}>
             <Form.Control
               type="search"
               placeholder="Busca por artista"
               className="me-2"
               aria-label="Search"
+              value = {value}
+              onChange={handleOnChange}
             />
-            <Button variant="outline-success" className='custom-btn'>Search</Button>
+            <Button variant="outline-success" 
+            className='custom-btn' 
+            type="submit" >
+                Search
+            </Button>
           </Form>
             <div>
         <CarWidget />
